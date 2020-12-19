02c9feb10f65ed64d6f8de4506618b156a6b0e91
andre barreto <alab.home@gmail.com>
2020-12-19 18:09:12 -0300

    Initial commit

---
+++ src/main/java/br/com/alabvox/ubaseapi/UbaseapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alabvox.ubaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbaseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbaseapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alabvox/ubaseapi/UbaseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alabvox.ubaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbaseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
